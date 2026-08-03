720928cc0dad4e57b01e52851c947380464c0e36
Andres <alcivar.andres001@hotmail.com>
2026-08-02 23:14:10 -0500

    actualizaciones para render

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,37 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  // 🔹 Configuración Swagger
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // Configuración CORS para REST
+  app.enableCors({
+    origin: '*',
+  });
+
+  // Configuración Swagger
   const config = new DocumentBuilder()
     .setTitle('Indriver NestJS API')
     .setDescription('Documentación de la API')
     .setVersion('1.0')
-    .addBearerAuth() // JWT para proteger mis endpoints
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  // Render proporciona el puerto mediante process.env.PORT
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Servidor ejecutándose en el puerto ${port}`);
 }
+
 bootstrap();

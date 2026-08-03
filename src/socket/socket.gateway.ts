1636114aeccd810a09d020548586a5badb34b59c
Andres <alcivar.andres001@hotmail.com>
2026-08-02 22:29:25 -0500

    prueba deconexion 2

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -13,36 +13,33 @@ import { Server, Socket } from 'socket.io';
   cors: {
     origin: '*',
   },
+  transports: ['websocket'],
 })
 export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  //OnGatewayDisconnect: Se ejecuta cuando un cliente se desconecta del gateway de sockets.
-  handleDisconnect(client: Socket) {
-    console.log(`Cliente desconectado de SOCKET IO: ${client.id}`);
+  handleConnection(client: Socket) {
+    console.log(`Cliente conectado de Socket.IO: ${client.id}`);
 
-    // Detecta cuándo se cierra la conexión subyacente.
     client.conn.on('close', (reason) => {
       console.log(`Conexión cerrada: ${client.id}`);
       console.log(`Motivo del cierre: ${reason}`);
     });
 
-    // Detecta errores del transporte.
     client.conn.on('error', (error) => {
       console.error(`Error de conexión: ${client.id}`, error);
     });
   }
 
-  //OnGatewayConnection: Se ejecuta cuando un cliente se conecta al gateway de sockets.
-  handleConnection(client: Socket, ...args: any[]) {
-    console.log(`Cliente conectado de SOCKET IO: ${client.id}`);
+  handleDisconnect(client: Socket) {
+    console.log(`Cliente desconectado de Socket.IO: ${client.id}`);
   }
 
   @SubscribeMessage('message')
   handleMessage(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
-    console.log('Nuevo mensaje :', data);
-    client.emit('new_message', 'Bien Gracias');
+    console.log('Nuevo mensaje:', data);
+    client.emit('new_message', 'Bien, gracias');
   }
 
   @SubscribeMessage('change_driver_position')
@@ -50,7 +47,8 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @ConnectedSocket() client: Socket,
     @MessageBody() data: any,
   ) {
-    console.log('Nueva posicion:', data);
+    console.log('Nueva posición:', data);
+
     client.emit('new_driver_position', {
       id: data.id,
       lat: data.lat,
